2e0dc75dda509d608ddd7013224127ec221c32e5
Yuhan Chen <chenyh@umich.edu>
2022-04-21 02:07:09 -0400

    custom lily58 keymaps

--- keyboards/lily58/lib/layer_state_reader.c
+++ keyboards/lily58/lib/layer_state_reader.c
@@ -4,7 +4,7 @@
 #include "lily58.h"
 
 #define L_BASE 0
-#define L_LOWER (1 << 1)
+#define L_MOVE (1 << 1)
 #define L_RAISE (1 << 2)
 #define L_ADJUST (1 << 3)
 #define L_ADJUST_TRI (L_ADJUST | L_RAISE | L_LOWER)
@@ -15,17 +15,10 @@ const char *read_layer_state(void) {
   switch (layer_state)
   {
   case L_BASE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Default");
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: BASE");
     break;
-  case L_RAISE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
-    break;
-  case L_LOWER:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
-    break;
-  case L_ADJUST:
-  case L_ADJUST_TRI:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Adjust");
+  case L_MOVE:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: MOVE");
     break;
   default:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%ld", layer_state);
